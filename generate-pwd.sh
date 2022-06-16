4dcebebc1977be6bd9f311882cb13789b735b100
Christian P. V. Christoffersen <88783438+br-cpvc@users.noreply.github.com>
2022-06-16 15:28:27 +0200

    added generate-pwd.sh

---
+++ generate-pwd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#from: https://www.redlever-solutions.com/blog/howto-generate-secure-passwords-with-openssl
+
+LENGTH=25
+if [ ! -z "$1" ] && [ $1 -gt 1 ]; then
+  LENGTH=$1
+fi
+NUMBYTES=`echo $LENGTH | awk '{print int($1*1.16)+1}'`
+
+openssl rand -base64 $NUMBYTES | tr -d "=+/" | cut -c1-$LENGTH
